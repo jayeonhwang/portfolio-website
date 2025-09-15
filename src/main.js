4a0ae62fc6d18d1c0c4627b47e10fabee6d08830
Jayeon Hwang <jayeon512@gmail.com>
2025-09-15 12:51:11 -0500

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -12,6 +12,18 @@ document.addEventListener('scroll', () => {
 
 // Make home section fade out when scroll down to next section
 const home = document.querySelector('.home__container');
-const homeHeigt = home.offsetHeight;
+const homeHeight = home.offsetHeight;
 
-document.addEventListener('scroll', () => { home.style.opacity = 1 - window.scrollY / homeHeigt });
+document.addEventListener('scroll', () => home.style.opacity = 1 - window.scrollY / homeHeight);
+
+
+//hide arrow-up button when window showing home
+
+const arrow = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrow.style.opacity = 1;
+  } else {
+    arrow.style.opacity = 0;
+  }
+});
